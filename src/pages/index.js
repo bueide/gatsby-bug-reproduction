1660646701ee0ecf7135546ec07e42008ca915a8
Eric Bueide <eric@buei.de>
2021-02-17 12:36:01 -0500

    Successfully reproduced error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
-import React from "react"
+import React, {useState} from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
-export default function Home() {
-  return <div>Hello world!</div>
+function ThemeToggler() {
+  const [theme, setTheme] = useState('light');
+  const nextTheme = theme === "light" ? "dark" : "light";
+  const sunMoon = theme === "light" ? <p>Moon</p> : <p>Sun</p>;
+  return (
+    <button
+      aria-label="Toggle Site Theme"
+      onClick={() => setTheme(nextTheme)}
+    >
+      {sunMoon}
+    </button>
+  );
 }
+
+export default function Home({ data }) {
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <nav><ThemeToggler /></nav>
+      <p>Hello world!</p>
+      <Img fluid={data.file.childImageSharp.fluid} />
+    </div>
+  )
+}
+
+export const data = graphql`
+    query {
+      file(relativePath: { eq: "gatsbytoast.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `
