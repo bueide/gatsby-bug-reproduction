24026da80e2e5bba7ce63024b9274934176175fd
Eric Bueide <eric@buei.de>
2021-02-18 11:58:06 -0500

    Added ThemeProvider and dependencies

---
+++ src/components/themes.js
@@ -0,0 +1,36 @@
+export const themes = {
+  light: {
+    colors: {
+      surface1: "#e6e6e6",
+      surface2: "#f2f2f2",
+      surface3: "#f9f9f9",
+      surface4: "#fbfbfb",
+      element1: "#111111",
+      element2: "#222222",
+      element3: "#333333",
+      element4: "#d8d8d8",
+      primary: "#04378b",
+      secondary: "#007cc4",
+      tertiary: "#00bed4",
+      quaternary: "#6bfbce",
+      pager: "#04378b",
+    },
+  },
+  dark: {
+    colors: {
+      surface1: "#262626",
+      surface2: "#333333",
+      surface3: "#C1CAE6",
+      surface4: "#101010",
+      element1: "#eeeeee",
+      element2: "#dddddd",
+      element3: "#cccccc",
+      element4: "#808080",
+      primary: "#262626",
+      secondary: "#98dbfe",
+      tertiary: "#61eef7",
+      quaternary: "#71faca",
+      pager: "#c1cae6",
+    },
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,61 @@
-import React, {useState} from "react";
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
+import React, { useState, useEffect } from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Global, ThemeProvider } from "@emotion/react"
+import { themes } from "../components/themes"
 
-function ThemeToggler() {
-  const [theme, setTheme] = useState('light');
-  const nextTheme = theme === "light" ? "dark" : "light";
-  const sunMoon = theme === "light" ? <p>Moon</p> : <p>Sun</p>;
+function ThemeToggler({ theme, onClick }) {
+  const nextTheme = theme === "light" ? "dark" : "light"
+  const sunMoon = theme === "light" ? <p>Moon</p> : <p>Sun</p>
   return (
-    <button
-      aria-label="Toggle Site Theme"
-      onClick={() => setTheme(nextTheme)}
-    >
+    <button aria-label="Toggle Site Theme" onClick={() => onClick(nextTheme)}>
       {sunMoon}
     </button>
-  );
+  )
 }
 
 export default function Home({ data }) {
+  const storedTheme = typeof window !== "undefined" && window.localStorage
+  const [theme, setTheme] = useState(
+    storedTheme ? localStorage.getItem("theme") : "light"
+  )
+  useEffect(() => {
+    localStorage.setItem("theme", theme)
+  }, [theme])
+  console.log(theme)
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-      <nav><ThemeToggler /></nav>
+    <ThemeProvider theme={themes[theme]}>
+      <Global
+        styles={theme => ({
+          body: {
+            margin: "3rem auto",
+            maxWidth: 600,
+            background: theme.colors.surface1,
+            color: theme.colors.element1
+          }
+          
+        })}
+      />
+      <nav>
+        <ThemeToggler
+          theme={theme}
+          onClick={nextTheme => setTheme(nextTheme)}
+        />
+      </nav>
       <p>Hello world!</p>
       <Img fluid={data.file.childImageSharp.fluid} />
-    </div>
+    </ThemeProvider>
   )
 }
 
 export const data = graphql`
-    query {
-      file(relativePath: { eq: "gatsbytoast.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 600) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
+  query {
+    file(relativePath: { eq: "gatsbytoast.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
-  `
+  }
+`
